f8cdf1d4b55fe51ffa447ed893246b8bdfaeb1da
Arrcturus <abringhentim@gmail.com>
2025-02-12 20:58:50 +0100

    Changes made and bugs solved

--- models/contract.py
+++ models/contract.py
@@ -5,10 +5,11 @@ class Contract(models.Model):
     _name = 'laliga.contract'
     _description = 'Contract'
 
+    name = fields.Char(string="Contract")
     wage = fields.Monetary('Wage', required=True, currency_field='currency_id')
     currency_id = fields.Many2one('res.currency', default=lambda self: self.env.ref('base.EUR'))
     start = fields.Date('Start')
     end = fields.Date('End')
 
-    player_id = fields.Many2one('laliga.player', string='employee')
-    staff_id = fields.Many2one('laliga.staff', 'contract', string='employee')
+    #player_id = fields.Many2one('laliga.player', string='employee')
+    staff_id = fields.Many2one('laliga.staff', string='employee')
--- models/player.py
+++ models/player.py
@@ -11,16 +11,19 @@ class Player(models.Model):
         ('1', 'Goalkeeper'),
         ('2', 'Defender'),
         ('3', 'Midfielder'),
-        ('4', 'Attacker yatusae')
+        ('4', 'Attacker')
     ], string='Position', default = '1')
     height = fields.Integer(string='Height (cm)', required=True)
     weight = fields.Integer(string='Weight (kg)', required=True)
     value = fields.Float(string='Player value', required=True)
     medicalRecord = fields.Many2many(string='Medical record', comodel_name='laliga.injury')
     transfers = fields.Many2many(string='Transfer record', comodel_name='laliga.transfer')
-    stats = fields.Many2one(comodel_name='laliga.stats', string='Stats', required=True)
-    contract = fields.One2many('laliga.contract', 'player_id', string="Contract")
-    min_wage = fields.Monetary('Minimun Wage', required=True, currency_field='currency_id')
+    stats = fields.Many2one(comodel_name='laliga.stats', string='Stats', required=False)
+    contract = fields.Many2one('laliga.contract', string="Contract")
+    wage = fields.Monetary(related="contract.wage")
+    start = fields.Date(related="contract.start")
+    end = fields.Date(related="contract.end")
+    min_wage = fields.Monetary('Minimun Wage', required=False, currency_field='currency_id')
     currency_id = fields.Many2one('res.currency', default=lambda self: self.env.ref('base.EUR'))
 
     team = fields.Many2one('laliga.team', string="Team")
